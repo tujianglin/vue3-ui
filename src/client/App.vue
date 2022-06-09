49986040cdc4025569a5699158b10e50830850ca
涂江林 <5300704+tujianglin@user.noreply.gitee.com>
2022-06-09 15:35:14 +0800

    feat: 静态文档

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app"> </div>
+  <div id="app">
+    <TButton name="1" />
+  </div>
 </template>
 <script lang="tsx" setup></script>
 <style lang="less" scoped></style>
--- src/client/main.ts
+++ src/client/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import { createUI } from 'tjl-ui';
+import 'tjl-ui/dist/style.css';
 
-createApp(App).mount('#app');
+createApp(App).use(createUI()).mount('#app');
